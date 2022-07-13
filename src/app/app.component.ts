3e2ce6dcfe86b7122a2cbe575a875be3ebf94071
Woga65 <woga@mytsa.de>
2022-07-13 10:56:01 +0200

    get and set data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { Firestore, collectionData, collection, setDoc, doc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'firebase-todolist';
+  todos$: Observable<any>;
+  todos: Array<any>;
+  todoText: string = '';
+
+  constructor(private firestore: Firestore) {
+    const coll = collection(this.firestore, 'todos');
+    this.todos$ = collectionData(coll);
+
+    this.todos$.subscribe(( newTodos ) => {
+      console.log('Neue Daten sind verfügbar: ', newTodos);
+      this.todos = newTodos;
+    });
+  }
+
+  addTodo() {
+    const coll = collection(this.firestore, 'todos');
+    setDoc(doc(coll), { name: this.todoText });
+    // setDoc(doc(coll, 'unique ID goes here'), { name: this.todoText });
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'firebasetodo20220707',
+    appId: '1:346214244529:web:a2e43650efabae99744290',
+    storageBucket: 'firebasetodo20220707.appspot.com',
+    apiKey: 'AIzaSyBvZBsm7iBCA42sCt3O7AZP45HD3k-LwSQ',
+    authDomain: 'fir-todo20220707.firebaseapp.com',
+    messagingSenderId: '346214244529',
+  },
   production: false
 };
 
